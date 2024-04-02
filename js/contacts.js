a09d8e4c6b9eca972654c909647e55ccffbb3ced
HomeBjo <bjoernteneicken@gmail.com>
2024-04-02 16:23:25 +0200

    Update contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -127,7 +127,7 @@ async function generateContactInSmall() {
 /**
  * This function sorts the names of all contacts and displays them alphabetically 
  */
-function sortByFirstLetter(contacts) {
+async function sortByFirstLetter(contacts) {
     return contacts.sort((a, b) => {
         const nameA = a.name.toUpperCase();
         const nameB = b.name.toUpperCase();
